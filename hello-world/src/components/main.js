91ef4288863d5e3bd58b3a66337140aecc4fbf5c
vsp <vivek.hhl@gmail.com>
2021-11-01 00:31:30 +0100

    first commit Gatsby site

---
+++ hello-world/src/components/main.js
@@ -0,0 +1,24 @@
+import React from "react"
+import Header from "./header"
+
+
+export default function Main({children}){
+
+	return(
+
+		<div>
+
+			<Header>
+
+
+			</Header>
+
+			{children}
+			
+		</div>
+
+	)
+
+
+}
+
